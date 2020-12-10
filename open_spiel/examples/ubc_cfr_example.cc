bab46f38ac5faea8b3ba3881328571485efcd6c1
newmanne <newman.n8@gmail.com>
2020-12-09 17:06:02 -0800

    Split out your own cfr example

--- open_spiel/examples/ubc_cfr_example.cc
+++ open_spiel/examples/ubc_cfr_example.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <string>
+#include <random>
 
 #include "open_spiel/abseil-cpp/absl/flags/flag.h"
 #include "open_spiel/abseil-cpp/absl/flags/parse.h"
@@ -20,28 +21,50 @@
 #include "open_spiel/algorithms/tabular_exploitability.h"
 #include "open_spiel/spiel.h"
 #include "open_spiel/spiel_utils.h"
+#include "open_spiel/game_transforms/turn_based_simultaneous_game.h"
+
+constexpr int kSeed = 230398247;
 
 ABSL_FLAG(std::string, game_name, "kuhn_poker", "Game to run CFR on.");
 ABSL_FLAG(int, num_iters, 1000, "How many iters to run for.");
 ABSL_FLAG(int, report_every, 100, "How often to report exploitability.");
+ABSL_FLAG(bool, turn_based, false, "Is the game turn based?");
 
 // Example code for using CFR+ to solve Kuhn Poker.
 int main(int argc, char** argv) {
   absl::ParseCommandLine(argc, argv);
-  std::shared_ptr<const open_spiel::Game> game =
-      open_spiel::LoadGame(absl::GetFlag(FLAGS_game_name));
+  std::shared_ptr<const open_spiel::Game> game;
+  std::mt19937 rng(kSeed);
+
+
+if (absl::GetFlag(FLAGS_turn_based)) {
+  game = open_spiel::LoadGameAsTurnBased(absl::GetFlag(FLAGS_game_name));
+} else {
+  game = open_spiel::LoadGame(absl::GetFlag(FLAGS_game_name));
+}
+
   open_spiel::algorithms::CFRSolver solver(*game);
-  std::cerr << "Starting CFR on " << game->GetType().short_name
+  std::cerr << "Starting CFR and CFR+ on " << game->GetType().short_name
             << "..." << std::endl;
 
   for (int i = 0; i < absl::GetFlag(FLAGS_num_iters); ++i) {
     solver.EvaluateAndUpdatePolicy();
     if (i % absl::GetFlag(FLAGS_report_every) == 0 ||
         i == absl::GetFlag(FLAGS_num_iters) - 1) {
-      double exploitability = open_spiel::algorithms::Exploitability(
+      double exploitability = open_spiel::algorithms::NashConv(
           *game, *solver.AveragePolicy());
-      std::cerr << "Iteration " << i << " exploitability=" << exploitability
+      std::cerr << "Iteration " << i << " NashConv=" << exploitability
                 << std::endl;
+
+      // const std::shared_ptr<open_spiel::Policy> average_policy = solver.AveragePolicy();
+
+      // // std::cout << PrintPolicy(average_policy) << std::endl;
+
+      // double nash_conv = open_spiel::algorithms::NashConv(*game, *average_policy, true);
+      // std::cout << "Game: " << absl::GetFlag(FLAGS_game_name) << ", iters = " << i
+      //           << ", NashConv: " << nash_conv << std::endl;
+
+
     }
   }
 }
