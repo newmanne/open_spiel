8dca8e067e538644f191ccf6599a03d4d4142e73
AWS ParallelCluster user <ubuntu@ip-10-255-7-68.ca-central-1.compute.internal>
2022-03-02 06:34:40 +0000

    Auction tree viewer stuffs

--- open_spiel/python/examples/straightforward_agent.py
+++ open_spiel/python/examples/straightforward_agent.py
@@ -26,69 +26,69 @@ class StraightforwardAgent(AbstractAgent):
         information_state_tensor = np.array(time_step.observations["info_state"][self.player_id])
 
         # Grab from encoding
-        cpi = clock_profit_index(self.num_players, self.num_actions)
-        profits = information_state_tensor[cpi:cpi + self.num_actions]
+        # cpi = clock_profit_index(self.num_players, self.num_actions)
+        # profits = information_state_tensor[cpi:cpi + self.num_actions]
 
-        legal_profits = [profits[i] for i in legal_actions]
-        action = legal_actions[np.argmax(legal_profits)]
+        # legal_profits = [profits[i] for i in legal_actions]
+        # action = legal_actions[np.argmax(legal_profits)]
 
         # else:
-        #     # Skip our own player encoding
-        #     information_state_tensor = information_state_tensor[self.num_players:]
+        # Skip our own player encoding
+        information_state_tensor = information_state_tensor[self.num_players:]
 
-        #     # Grab the budget
-        #     budget = information_state_tensor[0]
+        # Grab the budget
+        budget = information_state_tensor[0]
 
-        #     # Grab the values
-        #     values = information_state_tensor[1:self.num_products + 1]
+        # Grab the values
+        values = information_state_tensor[1:self.num_products + 1]
 
-        #     # Tensor looks like: Submitted / Processed / Agg / Posted Price
-        #     posted_prices = information_state_tensor[information_state_tensor.nonzero()][-self.num_products:] # Hacky
-        #     current_clock_prices = np.array(posted_prices) * self.increment # The encoding doesn't have clock prices! Need to rederive them (multiply by self.increment)
+        # Tensor looks like: Submitted / Processed / Agg / Posted Price
+        posted_prices = information_state_tensor[information_state_tensor.nonzero()][-self.num_products:] # Hacky
+        current_clock_prices = np.array(posted_prices) * self.increment # The encoding doesn't have clock prices! Need to rederive them (multiply by self.increment)
 
 
-        #     # TODO: This isn't really a MIP - you just literally pick the best bundle. It's a max function. Do you need MIP power later, or should you rewrite this?
+        # TODO: This isn't really a MIP - you just literally pick the best bundle. It's a max function. Do you need MIP power later, or should you rewrite this?
 
-        #     # Convert legal actions into packages and only write the MIP over these packages => Budget + Eligibility taken care of, not MIP constraints
-        #     expected_profits = []
-        #     var_id_to_action = dict()
-        #     # Step 1: Get all of the bundles from legal actions. How much do they each cost? How much do I value each of them?
-        #     for var_id, action in enumerate(legal_actions):
-        #         bundle = self.action_to_bundle[action]
-        #         bundle_value = values @ bundle
-        #         bundle_price = current_clock_prices @ bundle
-        #         bundle_profit = bundle_value - bundle_price
-        #         expected_profits.append(bundle_profit)
-        #         var_id_to_action[var_id] = action
+        # Convert legal actions into packages and only write the MIP over these packages => Budget + Eligibility taken care of, not MIP constraints
+        expected_profits = []
+        var_id_to_action = dict()
+        # Step 1: Get all of the bundles from legal actions. How much do they each cost? How much do I value each of them?
+        for var_id, action in enumerate(legal_actions):
+            bundle = self.action_to_bundle[action]
+            bundle_value = values @ bundle
+            bundle_price = current_clock_prices @ bundle
+            bundle_profit = bundle_value - bundle_price
+            expected_profits.append(bundle_profit)
+            var_id_to_action[var_id] = action
 
-        #     # Extract current clock prices
-        #     # Extract my own values
-        #     # Extract my budget
-        #     # Place the bid that maximizes my profit at clock prices subject to budget (TODO: This is not sophisticated enough)
+        # Extract current clock prices
+        # Extract my own values
+        # Extract my budget
+        # Place the bid that maximizes my profit at clock prices subject to budget (TODO: This is not sophisticated enough)
 
-        #     feasible_result = True
-        #     problem = LpProblem(f"{self.player_id}_Bid", LpMaximize)
+        feasible_result = True
+        problem = LpProblem(f"{self.player_id}_Bid", LpMaximize)
 
-        #     bundle_variables = LpVariable.dicts("X", np.arange(len(expected_profits)), cat=LpBinary)
+        bundle_variables = LpVariable.dicts("X", np.arange(len(expected_profits)), cat=LpBinary)
 
-        #     # OBJECTIVE
-        #     problem += lpDot(expected_profits, bundle_variables.values())
+        # OBJECTIVE
+        problem += lpDot(expected_profits, bundle_variables.values())
 
-        #     # Constraint: Only 1 bundle
-        #     problem += lpSum(bundle_variables.values()) == 1
+        # Constraint: Only 1 bundle
+        problem += lpSum(bundle_variables.values()) == 1
 
-        #     try: 
-        #         problem.writeLP(f'straightforward.lp')
-        #         obj = pulp_solve(problem, save_if_failed=True)
-        #         for var_id in range(len(bundle_variables)):
-        #             if value(bundle_variables[var_id]) > .99: # Rounding stupidness
-        #                 action = var_id_to_action[var_id]
-        #                 break # Only 1 feasible bundle
-        #     except ValueError as e:
-        #         # if MIP is infeasible, drop out - TODO: Should this ever happen?
-        #         feasible_result = False
-        #         logging.warning(f'Failed to solve MIP; dropping out')
-        #         action = 0
+        try: 
+            problem.writeLP(f'straightforward.lp')
+            obj = pulp_solve(problem, save_if_failed=True)
+            for var_id in range(len(bundle_variables)):
+                if value(bundle_variables[var_id]) > .99: # Rounding stupidness
+                    action = var_id_to_action[var_id]
+                    break # Only 1 feasible bundle
+        except ValueError as e:
+            # if MIP is infeasible, drop out - TODO: Should this ever happen?
+            feasible_result = False
+            logging.warning(f'Failed to solve MIP; dropping out')
+            action = 0
         
         probs = np.zeros(self.num_actions)
         probs[action] = 1.0
